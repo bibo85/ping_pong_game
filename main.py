2d503c2d8a305731f16e51489bc479d1243e1215
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2021-10-06 22:10:24 +0500

    Ускоряем отрисовку. Рисуем центральную пунктирную линию. Скрываем в конце черепашку

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ playing_field_height = 600
 
 # рисуем игровое поле
 border = turtle.Turtle()
+border.speed(0)
 border.color('green')
 border.begin_fill()
 border.goto(-500, 300)
@@ -22,5 +23,19 @@ border.goto(500, -300)
 border.goto(-500, -300)
 border.goto(-500, 300)
 border.end_fill()
+
+# рисуем центральную линию
+border.goto(0, 300)
+border.color('white')
+border.setheading(270)
+for i in range(25):
+    if i % 2 == 0:
+        border.forward(24)
+    else:
+        border.up()
+        border.forward(24)
+        border.down()
+
+border.hideturtle()
 
 window.mainloop()
